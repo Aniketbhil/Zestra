538a9b322aa512784d14862d3bf34e16f652115f
KrehantGajjar10 <krehantgajjar101106@gmail.com>
2026-07-26 17:38:24 +0530

    frontend: initialize project structure, dependencies, and core design system

---
+++ zestra-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-[#10B981] text-white p-4 rounded-xl shadow-lg font-bold">Zestra Setup Successful</div>
+  )
+}
+
+export default App
